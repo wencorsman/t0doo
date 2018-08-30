4c9edc6028b7eaf2ddd03cc6b792410ed2cb487b
Max Black <wencorsman@gmail.com>
2018-08-30 15:12:55 +0300

    first

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('./public'));
+
+app.get('/', function (req, res) {
+    res.send(':))');
+})
+app.listen(8080);
